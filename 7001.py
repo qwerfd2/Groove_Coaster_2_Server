8ae153c2d67ec2759c7afbfc6bc15a85ea4d4646
UnitedAirforce <i5904503668i@gmail.com>
2025-02-22 08:49:36 +0800

    fix

--- 7001.py
+++ 7001.py
@@ -211,7 +211,7 @@ def hash_password(password):
     return hashed_password
 
 def verify_password(password, hashed_password):
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 def is_alphanumeric(username):
     pattern = r"^[a-zA-Z0-9]+$"
